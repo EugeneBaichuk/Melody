69fed42805aaa481dba5baa462caf1a32473261c
EugeneBaichuk <89223268+EugeneBaichuk@users.noreply.github.com>
2021-08-25 23:05:33 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,13 +3,25 @@ $(document).ready(function () {
   var floorPath = $(".home-image path"); // каждый отдельный этаж в SVG
   var counterUp = $(".counter-up"); /* кнопка увеличения этажа*/
   var counterDown = $(".counter-down"); /* кнопка уменьшения этажа*/
+  var modal = $(".modal");
+  var modalCloseButton = $(".modal-close-button");
+  var viewFlatsButton = $(".view-Flats");
+  var flatsPath = $(".flats path");
+  var flatLink = 1;
+
+  floorPath.on("click", toggleModal)
+  modalCloseButton.on("click", toggleModal)
+  viewFlatsButton.on("click", toggleModal)
 
   // функция при наведении мышью на этаж
   floorPath.on("mouseover", function () {
-  floorPath.removeClass("current-floor"); // удаляем активный класс у этажей
-  currentFloor = $(this).attr("data-floor"); // получаем значение текущего этажа 
-  $(".counter").text(currentFloor); // записываем значение этажа в счетчик справа
+    floorPath.removeClass("current-floor"); // удаляем активный класс у этажей
+    currentFloor = $(this).attr("data-floor"); // получаем значение текущего этажа 
+    $(".counter").text(currentFloor); // записываем значение этажа в счетчик справа
   });
+
+
+
 
   counterUp.on("click", function () { //отслеживаем клик по кнопке вверх
     if (currentFloor < 18) {
@@ -31,5 +43,8 @@ $(document).ready(function () {
     } 
   });
 
-
+  
+  function toggleModal (){
+    modal.toggleClass ("is-open");
+    } /* функция открыть-закрыть окно*/
 });
